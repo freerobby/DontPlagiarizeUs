c3036706e6a3993f8b8d9c1d84e79be5dc648e45
Robby Grossman <robby@freerobby.com>
2010-04-16 22:38:05 -0400

    Add @anywhere support.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,7 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
   def link_to_twitter_handle(screen_name)
-    link_to "@#{screen_name}", "http://twitter.com/#{screen_name}"
+    # liks via @anywhere
+    "@#{screen_name}"
   end
 end
